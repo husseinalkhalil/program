00173ec75751e1eec661f1b3ff2de51928267c36
husseinalkhalil <w.syriaonline@gmail.com>
2025-04-29 23:09:28 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,30 @@
-
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('pwa-cache').then((cache) => {
       return cache.addAll([
-        '/',
+        // الصفحة الرئيسية وملفات أساسية
         '/index.html',
-        '/style.css',
-        '/app.js',
-        '/icons/app-icon.png'  // تأكد من إضافة جميع الملفات التي يحتاجها التطبيق
+        '/manifest.json',
+        '/service-worker.js',
+
+        // تطبيق الحوالات
+        '/hawalat/index.html',
+        '/hawalat/view.html',
+        '/hawalat/script.js',
+        '/hawalat/style.css',
+        '/hawalat/fonts/Amiri-Regular.ttf',
+        '/hawalat/icons/logo.png',
+        '/hawalat/icons/real.png',
+
+        // تطبيق التوصيل
+        '/tawsil/index.html',
+        '/tawsil/view.html',
+        '/tawsil/manifest.json',
+        '/tawsil/sw.js',
+        '/tawsil/fonts/Amiri-Regular.ttf',
+        '/tawsil/icons/app-icon.png',
+        '/tawsil/icons/real.png',
+        '/tawsil/icons/trash-icon.png'
       ]);
     })
   );
